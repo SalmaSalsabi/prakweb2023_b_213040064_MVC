7c62474ad32634b45d4be7a7d32447e7bfa610a6
SalmaSalsabi <salmasalsabila073@gmail.com>
2023-10-22 15:25:11 +0700

    Vidio 7 Model

--- phpmvc/app/controllers/Home.php
+++ phpmvc/app/controllers/Home.php
@@ -1,13 +1,12 @@
 <?php
-
 class Home extends Controller {
-    public function index()
-        {
-          $data['judul'] = 'Home';
-          $this->view('templates/header', $data);
-          $this->view('home/index');
-          $this->view('templates/footer');
-        }
-
-
+    public function index() 
+    {
+        $data ['judul'] = 'Home';
+        $data ['nama'] = $this->model('User_model')->getUser();
+        $this->view('templates/header', $data);
+        $this->view('home/index');
+        $this->view('home/index', $data);
+        $this->view('templates/footer');
+    }
 }
---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Salma';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
